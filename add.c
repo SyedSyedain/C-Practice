72de05a19f5ed1d111c0b37d3899be2b6f71b445
SyedSyedain <syedain.dev@gmail.com>
2025-10-22 19:11:34 +0530

    formatted questions

--- add.c
+++ add.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int A , B;
-printf("enter the number A\n");
-scanf( "%d",&A);
-printf("enter the number B\n");
-scanf("%d",&B);
-int sum;
-sum = A + B;
-printf("the sum is :%d",sum);
-return 0;
+    int A, B;
+    printf("enter the number A\n");
+    scanf("%d", &A);
+    printf("enter the number B\n");
+    scanf("%d", &B);
+    int sum;
+    sum = A + B;
+    printf("the sum is :%d", sum);
+    return 0;
 }
--- addequal.c
+++ addequal.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main (void) {
-int a =10;
-a*=2;      //+=,-=,/=
-printf("%d",a);
-return 0;
+int main(void)
+{
+    int a = 10;
+    a *= 2; //+=,-=,/=
+    printf("%d", a);
+    return 0;
 }
